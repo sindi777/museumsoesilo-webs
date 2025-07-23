e5ac62387ff425905fbe77bc7870f6c6d88ff02e
sindi777 <sindy3545@gmail.com>
2025-07-24 00:27:48 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,42 @@
+// import { Geist, Geist_Mono } from "next/font/google"
+// import "./globals.css"
+// import LayoutClient from './componens/LayoutClient';
+// import SessionProviderWrapper from "./componens/SessionProviderWrapper";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// })
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// })
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <head>
+        
+//       </head>
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#EFE7DF] text-[#1E1E1E]`}
+//       >
+//        <SessionProviderWrapper>
+//           <LayoutClient>
+//             {children}
+//           </LayoutClient>
+//         </SessionProviderWrapper>
+//       </body>
+//     </html>
+//   )
+// }
+
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import LayoutClient from './componens/LayoutClient';
 import SessionProviderWrapper from "./componens/SessionProviderWrapper";
+import Script from 'next/script'; // ✅ Import Next.js Script component
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,11 +51,28 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        {/* ✅ Script Hotjar */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3705066,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#EFE7DF] text-[#1E1E1E]`}
       >
-       <SessionProviderWrapper>
+        <SessionProviderWrapper>
           <LayoutClient>
             {children}
           </LayoutClient>
