ccbb6a8995c2edca7e31a21ce3fc59c0aab396f9
sindi777 <sindy3545@gmail.com>
2025-07-24 00:54:47 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,21 +53,20 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* ✅ Script Hotjar */}
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3705066,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `
-          }}
-        />
+        <Script id="hotjar-init" strategy="afterInteractive">
+  {`
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:3705066,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `}
+</Script>
+
+          
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#EFE7DF] text-[#1E1E1E]`}
