5b02ff5a593ba55884a4cbcfc82a10d6c0d375e0
sindi777 <sindy3545@gmail.com>
2025-07-30 16:22:32 +0700

    2

--- src/app/detail-koleksi/page.jsx
+++ src/app/detail-koleksi/page.jsx
@@ -1,11 +1,15 @@
 import { prisma } from '@/lib/prisma'
 import VirtualCollectionClient from './VirtualCollectionClient'
 
+// TAMBAHKAN BARIS INI
+export const revalidate = 0;
+
 export default async function VirtualCollectionPage() {
-  const koleksi = await prisma.koleksi.findMany({})
+  const koleksi = await prisma.koleksi.findMany({
+    orderBy: {
+      createdAt: 'desc' // Tambahkan urutan agar data terbaru di atas
+    }
+  })
 
   return <VirtualCollectionClient koleksi={koleksi} />
 }
-
-
-
